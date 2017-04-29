66f1509b72390abe4ed0d963c31f63cdd39ec491
slstein14 <slstein14@my.trine.edu>
2017-04-29 17:51:37 -0400

    Added functions to make and parse JSON strings

--- uiid/uiid/apps/core/views.py
+++ uiid/uiid/apps/core/views.py
@@ -3,8 +3,23 @@ from django.http import HttpResponse
 from django.shortcuts import get_object_or_404, render_to_response
 from django.template import RequestContext
 from django.contrib import messages
-
+import json
 
 def index(request):
 	return HttpResponse('UIID')
 
+def jsonParse(jsonStr):
+	#Takes in a json structure  of form '{"User_Type":"val1","User_Query":"val2","Rated_Url":"val3"}' 
+	#and parses into an array of 3 Python variables [val1,val2,val3]
+	jsonData=json.loads(jsonStr)
+	User_Type=jsonData['User_Type']
+	User_Query=jsonData['User_Query']
+	Rated_URL=jsonData['Rated_Url']
+	jsonList=[User_Type,User_Query,Rated_URL]
+
+	return jsonList
+
+def jsonForm(user_type,user_query,rated_url):
+	#Takes 3 strings and concatenates into a json string
+	jsonSTR='{"User_Type":"'+user_type+'","User_Query":"'+user_query+'","Rated_Url":"'+rated_url+'"}'
+	return jsonSTR
